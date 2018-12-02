a5da1c0982936981cd6d6dde440f06612607a369
SHB743 <shoaib.devacc@gmail.com>
2018-12-02 20:55:05 +0000

    Added colors for visualization

--- Game.py
+++ Game.py
@@ -35,6 +35,8 @@ Player.decel = 10
 Player.gravity = 10
 
 #Ai Stuffs
+high_score = 0
+highest_scorer = None
 batch_size = 100
 AIs = []
 def create_networks():
@@ -61,6 +63,8 @@ else:
 
 def Run(timeout = 10.0):
 	global AIs
+	global high_score
+	global highest_scorer
 	old_time = (pygame.time.get_ticks()/100.00)
 	time_delta = 1.0
 	while (timeout > 0):
@@ -71,6 +75,15 @@ def Run(timeout = 10.0):
 		for AI in AIs:
 			AI[1].fire([ (AI[0].player_rect.y)/(height*1.0) ])
 			AI[0].update(time_delta)
+			#visuals
+			if (highest_scorer):
+				highest_scorer.color = highest_scorer.oldcolor
+			if (AI[0].score > high_score):
+				high_score = AI[0].score
+				highest_scorer = AI[0]
+			highest_scorer.oldcolor = highest_scorer.color
+			highest_scorer.color = 255,255,255
+			#visuals*
 		#Update AIs*
 		# The guidelines
 		pygame.draw.line(screen,(244,244,66),[0,height/2],[width,height/2],1)
@@ -82,6 +95,7 @@ def Run(timeout = 10.0):
 		#print(f"FPS:{int(1/(time_delta/10))}" )
 		timeout -= (time_delta/10)
 		#Time Stuffs*
+	high_score = 0
 
 
 
@@ -100,6 +114,7 @@ def train(epochs=10, timeout=10.0, retention=10.0, children=5):
 		retainedAISize = len(AIs)
 		print(f"keeping top {retention}% of AIs : {retainedAISize}")
 		for AI in AIs:
+			AI[0].color = 0,0,255
 			print(AI[0].score)
 			AI[0].score = 0
 		print("\n\n\n")
@@ -112,6 +127,7 @@ def train(epochs=10, timeout=10.0, retention=10.0, children=5):
 				AIs.append( (Player.Player(),deepcopy(AIs[i][1]))  )
 				AIs[-1][1].nodes[-1][0].output_funct = AIs[-1][0].jump
 				AIs[-1][1].mutate()
+				AIs[-1][0].color = 0,255,0
 			AIs[i][1].nodes[-1][0].output_funct = tmp
 		#All retained AIs should have y mutated copies*
 		#Fill up remaining batch size with new copies
@@ -125,7 +141,7 @@ def train(epochs=10, timeout=10.0, retention=10.0, children=5):
 		#Fill up remaining batch size with new copies*
 
 if (len(fname) < 3):
-	train(timeout=10 , epochs=6)
+	train(timeout=20 , epochs=3 )
 
 print("Showing the best one")
 AIs = AIs[:1]
